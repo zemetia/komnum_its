bc8f920bf6f41aff1c9e7e755e6a2ea5109b60ca
Aryan Shafa Wardana <aryan.s.wardana@gmail.com>
2022-10-29 00:09:20 +0700

    feat: Fixed Point Iteration finished

--- module/terbuka/fixed_point_iteration.py
+++ module/terbuka/fixed_point_iteration.py
@@ -1,3 +1,15 @@
 class FixedPointIteration:
-    def __init__(self):
+    def __init__(self, func, point:float = 0):
+        self.func = func
+        self.point = point
+        self.data = {}
+        self.generate()
+    
+    def compute_next(self):
+        return self.func(self.data["point"][-1])
+
+    def generate(self, iteration:int = 5):
+        self.data["point"] = [self.point]
+        for i in range(iteration):
+            self.data["point"].append(self.compute_next())
         pass
