84fc2dfa6d18e266048e52961ec4c2879ffbbbd2
Yoel Sitorus <zemetia>
2022-10-29 08:11:02 +0700

    feat: add feature to Kuadratur and Trapezoida that can go with a dataset

--- module/integrasi/kuadratur.py
+++ module/integrasi/kuadratur.py
@@ -2,19 +2,40 @@ import math
 
 #lebih bagus jadi fungsi
 class Kuadratur:
-    def __init__(self, func, bottomBound:float = 1, topBound:float = 3):
+    def __init__(self):
+        self.boundOne = 1/math.sqrt(3)
+        self.type = "none"
+
+    def checkType(self, type):
+        return self.type == type
+
+    def setByFunction(self, func, bottomBound:float = 1, topBound:float = 3):
         self.func = func
         self.bottomBound = bottomBound
         self.topBound = topBound
-        self.boundOne = 1/math.sqrt(3)
-        self.data = {}
+        self.type = "function"
+
+    def setByDatas(self, x: list = [], y: list = []):
+        if len(x) != len(y):
+            return
+        self.bottomBound = x[0]
+        self.f_bottomBound = y[0]
+        self.topBound = x[-1]
+        self.f_topBound = y[-1]
+        self.type = "datas"
 
     def computeIntegration(self) -> float:
+        if self.checkType("none"):
+            print("there is no datas/function")
+            return 
+            
         tpb = (self.topBound + self.bottomBound) / 2
         tmb = (self.topBound - self.bottomBound) / 2
         x0 = tpb - (tmb * self.boundOne)
         x1 = tpb + (tmb * self.boundOne)
 
-        result = (self.func(x0) * tmb) + (self.func(x1) * tmb)
-        return result
+        if self.checkType("function"): result = (self.func(x0) * tmb) + (self.func(x1) * tmb)
+        elif self.checkType("datas"): result = (self.f_bottomBound * tmb) + (self.f_topBound * tmb)
+
+        return result 
 
--- module/integrasi/trapezoida.py
+++ module/integrasi/trapezoida.py
@@ -1,11 +1,31 @@
 import numpy as np
 
 class Trapezoida:
-    def __init__(self, func, bottomBound:float = 1, topBound:float = 3):
+    def __init__(self):
+        self.type = "none"
+
+    def checkType(self, type):
+        return self.type == type
+
+    def setByFunction(self, func, bottomBound:float = 1, topBound:float = 3):
         self.func = func
         self.bottomBound = bottomBound
         self.topBound = topBound
-        self.data = {}
+        self.type = "function"
+
+    def setByDatas(self, x: list = [], y: list = []):
+        if len(x) != len(y):
+            print("the x size and y size is different")
+            return
+        self.bottomBound = x[0]
+        self.f_bottomBound = y[0]
+        self.datasY = y[1:-1] 
+        self.topBound = x[-1]
+        self.f_topBound = y[-1]
+        self.type = "datas"
+
+    def getDeltaX(self):
+        return abs(self.topBound - self.bottomBound)
 
     def createSetData(self, section:int) -> float:
         # num=section+1; ex: we need 5 values beetwen bottom and top bound
@@ -17,15 +37,37 @@ class Trapezoida:
         return data
 
     def computeIntegration(self, section:int) -> float:
-        center = sum(self.createSetData)
-        result = (1/2) * (self.func(self.bottomBound) + 2*(center) + self.func(self.topBound))
+        if self.checkType("function"):
+            center = sum(self.createSetData(section))
+            f_bottom = self.func(self.bottomBound)
+            f_top = self.func(self.topBound)
+        elif self.checkType("datas"):
+            center = sum(self.datasY)
+            f_bottom = self.f_bottomBound
+            f_top = self.f_topBound
+        else: return
+
+        result = (self.getDeltaX()/2) * (f_bottom + 2*(center) + f_top)
+
         return result
     
     def computeTipCorrection(self, section:int) -> float:
-        tip = (1/12) * (self.func(self.topBound) - self.func(self.bottomBound))
-        return self.computeIntegration - tip
+        if self.checkType("function"):
+            f_bottom = self.func(self.bottomBound)
+            f_top = self.func(self.topBound)
+        elif self.checkType("datas"):
+            f_bottom = self.f_bottomBound
+            f_top = self.f_topBound
+        else: return 
+
+        tip = (self.getDeltaX()/12) * (f_top - f_bottom)
+        return self.computeIntegration(section) - tip
 
     def generate(self, section:int = 5) -> float:
+        if self.checkType("none"):
+            print("there is no datas/function")
+            return 
+
         self.data = {
             "integrantion": self.computeIntegration(section),
             "tipCorrection": self.computeTipCorrection(section)
